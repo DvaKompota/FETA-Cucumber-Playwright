dbe78dafe6e0c99f826153201e74d3007634a4f3
Sergey Kolokolov <serg.a.bells@gmail.com>
2022-11-19 17:49:32 -0800

    adds a GET API test for Swagger project

---
+++ tests/swagger/api/pet.spec.ts
@@ -0,0 +1,64 @@
+import { test, expect, type Page } from '@playwright/test';
+
+const baseURL = 'https://petstore3.swagger.io/api/v3';
+
+test.describe('Pet API testing', () => {
+
+    test('Find pet by ID: GET /pet/{petId}', async ({ request }) => {
+        const payload: Pet = await generatePetObject();
+        await request.post(`${baseURL}/pet`, { data: payload });
+        const uri:string = `pet/${payload.id}`;
+        const response = await request.get(`${baseURL}/${uri}`);
+        expect (response.status()).toBe(200);
+        const responseBody = JSON.parse(await response.text())
+        expect (responseBody.id).toBe(payload.id)
+        expect (responseBody.category.id).toBe(payload.category.id)
+        expect (responseBody.category.name).toBe(payload.category.name)
+        expect (responseBody.name).toBe(payload.name)
+        expect (responseBody.status).toBe(payload.status)
+    });
+
+});
+
+async function generatePetObject(): Promise<Pet> {
+    const epoch = Date.now()
+    const petObject: Pet = {
+        "id": epoch,
+        "name": `bobik${epoch}`,
+        "category": {
+        "id": 1,
+        "name": "Dogs"
+        },
+        "photoUrls": [
+        `https://photos.google.com/photo/bobik${epoch}1`,
+        `https://photos.google.com/photo/bobik${epoch}2`,
+        ],
+        "tags": [
+        {
+            "id": epoch,
+            "name": `bobik${epoch}`
+        }
+        ],
+        "status": "available"
+    }
+    return petObject
+};
+
+export interface Pet {
+    id:	number;
+    name: string;
+    category: Category;
+    photoUrls: string[];
+    tags: Tag[];
+    status: 'available' | 'pending' | 'sold';
+}
+
+export interface Category {
+    id:	number;
+    name: string;
+}
+
+export interface Tag {
+    id:	number;
+    name: string;
+}
