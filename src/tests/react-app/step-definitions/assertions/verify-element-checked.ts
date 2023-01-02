6f11de2defc4c06f990bc5ecbc2b344baff94a5c
Sergey Kolokolov <sergey@mosaic.tech>
2023-01-01 18:31:13 -0800

    adds `the element should be checked` step

---
+++ src/tests/react-app/step-definitions/assertions/verify-element-checked.ts
@@ -0,0 +1,25 @@
+import { Then } from '@cucumber/cucumber'
+import { ScenarioWorld } from '../../setup/world';
+import { ElementKey } from '../../../../global-config';
+import { getElementLocator } from '../../../../resources/common/web-element-helper';
+import { waitFor } from '../../../../resources/common/wait-for-behavior';
+
+Then(
+    /^the "([^"]*)" should (not )?be checked$/,
+    async function( this: ScenarioWorld, elementKey: ElementKey, negate: boolean ) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+
+        console.log(`the ${elementKey} should ${negate?'not ':''}be checked`);
+
+        const elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor( async () => {
+            const isElementChecked: boolean = await page.isChecked(elementIdentifier)
+            return isElementChecked === !negate;
+        });
+
+    }
+)
