1558ca09d320039de474b500b835b7fce32bc171
Sergey Kolokolov <sergey@mosaic.tech>
2023-01-03 13:59:09 -0800

    adds the `element should contain the value` step

--- src/tests/react-app/step-definitions/assertions/verify-element-value.ts
+++ src/tests/react-app/step-definitions/assertions/verify-element-value.ts
@@ -3,6 +3,7 @@ import { ScenarioWorld } from '../../setup/world';
 import { ElementKey } from '../../../../global-config';
 import { getElementLocator } from '../../../../resources/common/web-element-helper';
 import { waitFor } from '../../../../resources/common/wait-for-behavior';
+import { getValue } from '../../../../resources/common/html-behavior';
 
 Then(
     /^the "([^"]*)" should (not )?contain the text "([^"]*)"$/,
@@ -19,6 +20,32 @@ Then(
         await waitFor( async () => {
             const elementText: string | null = await page.textContent(elementIdentifier);
             return elementText?.includes(expectedElementText) === !negate;
+        });
+
+    }
+)
+
+Then(
+    /^the "([^"]*)" ([a-z]* )?should (not )?contain the value "([^"]*)"$/,
+    async function(
+        this: ScenarioWorld,
+        elementKey: ElementKey, 
+        elementType: string,
+        negate: boolean,
+        expectedValue: string 
+    ) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+
+        console.log(`the ${elementKey} ${elementType?elementType:''}should ${negate?'not ':''}contain the value ${expectedValue}`);
+
+        const elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
+
+        await waitFor( async () => {
+            const elementAttribute: string | null = await getValue(page, elementIdentifier);
+            return elementAttribute?.includes(expectedValue) === !negate;
         });
 
     }
