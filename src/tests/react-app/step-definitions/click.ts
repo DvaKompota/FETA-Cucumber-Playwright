f9edf7e5cb092870dd7cc593c8ae7076e33732a4
Sergey Kolokolov <serg.a.bells@gmail.com>
2023-01-04 19:36:33 -0800

    adds the `click element with text equal to` step

--- src/tests/react-app/step-definitions/click.ts
+++ src/tests/react-app/step-definitions/click.ts
@@ -29,18 +29,53 @@ When(
 )
 
 When(
-    /^I click the "([^"]*)" containing the "(.*)" text$/,
-    async function( this: ScenarioWorld, elementKey: ElementKey, elementText: string ) {
+    /^I click the "([^"]*)" ([a-z]* )?containing the "(.*)" text$/,
+    async function(
+        this: ScenarioWorld,
+        elementKey: ElementKey, 
+        elementType: string,
+        elementText: string
+    ) {
         const {
             screen: { page },
             globalConfig,
         } = this;
     
-        console.log(`I click the ${elementKey} containing the "${elementText}" text`);
+        console.log(`I click the ${elementKey} ${elementType?elementType:''}containing the "${elementText}" text`);
 
         const elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
 
         const selectorWithText: string = `${elementIdentifier}:has-text("${elementText}")`;
+
+        await waitFor( async () => {
+            const result = await page.waitForSelector(selectorWithText, { state: 'visible' });
+            if (result) {
+                await clickElement(page, selectorWithText);
+            }
+            return result;
+        });
+
+    }
+)
+
+When(
+    /^I click the "([^"]*)" ([a-z]* )?with text equal to "(.*)"$/,
+    async function(
+        this: ScenarioWorld,
+        elementKey: ElementKey, 
+        elementType: string,
+        elementText: string
+    ) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+    
+        console.log(`I click the ${elementKey} ${elementType?elementType:''}with text equal to "${elementText}"`);
+
+        const elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
+
+        const selectorWithText: string = `${elementIdentifier}:text-is("${elementText}")`;
 
         await waitFor( async () => {
             const result = await page.waitForSelector(selectorWithText, { state: 'visible' });
