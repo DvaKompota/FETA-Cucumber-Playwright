08b283964c2c3912a6c47a312c8f2ee55bd3b4ee
Sergey Kolokolov <serg.a.bells@gmail.com>
2023-01-21 13:30:37 -0800

    updates the `I (check|uncheck) the element` step

--- src/tests/react-app/step-definitions/check.ts
+++ src/tests/react-app/step-definitions/check.ts
@@ -1,26 +1,32 @@
 import { When } from '@cucumber/cucumber'
 import { ElementKey } from '../../../global-config'
 import { ScenarioWorld } from '../setup/world';
-import { checkElement } from '../../../resources/common/html-behavior';
+import { checkElement, uncheckElement } from '../../../resources/common/html-behavior';
 import { waitFor } from '../../../resources/common/wait-for-behavior';
 import { getElementLocator } from '../../../resources/common/web-element-helper';
 
 When(
-    /^I check the "([^"]*)"$/,
-    async function( this: ScenarioWorld, elementKey: ElementKey ) {
+    /^I (check|uncheck) the "([^"]*)"$/,
+    async function( this: ScenarioWorld, action: string, elementKey: ElementKey ) {
         const {
             screen: { page },
             globalConfig,
         } = this;
     
-        console.log(`I click the ${elementKey}`);
+        console.log(`I ${action} the ${elementKey}`);
 
         const elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
+
+         const shouldBeChecked: boolean = (action == 'check');
 
         await waitFor( async () => {
             const result = await page.waitForSelector(elementIdentifier, { state: 'visible' });
             if (result) {
-                await checkElement(page, elementIdentifier);
+                if (shouldBeChecked) {
+                    await checkElement(page, elementIdentifier);
+                } else {
+                    await uncheckElement(page, elementIdentifier);
+                }
             }
             return result;
         });
