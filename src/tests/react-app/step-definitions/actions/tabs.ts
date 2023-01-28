421c2ccb401788f3ca760d84d80dbd7a7fc4f66f
Sergey Kolokolov <serg.a.bells@gmail.com>
2023-01-28 08:33:21 -0800

    adds `fill the element on n-th tab with text` step

---
+++ src/tests/react-app/step-definitions/actions/tabs.ts
@@ -0,0 +1,36 @@
+import { When } from '@cucumber/cucumber'
+import { ElementKey } from '../../../../global-config'
+import { ScenarioWorld } from '../../setup/world';
+import { waitFor } from '../../../../resources/common/wait-for-behavior';
+import { getElementLocator } from '../../../../resources/common/web-element-helper';
+import { fillTextOnPage } from '../../../../resources/common/html-behavior';
+
+When(
+    /^I fill the "([^"]*)" ([a-z]* )?on the "([0-9]+st|[0-9]+nd|[0-9]+rd|[0-9]+th)" tab with the "(.*)" text$/,
+    async function(
+        this: ScenarioWorld,
+        elementKey: ElementKey, 
+        elementType: string,
+        tabPosition: string,
+        textInput: string 
+    ) {
+        const {
+            screen: { page, context },
+            globalConfig,
+        } = this;
+    
+        console.log(`I fill the ${elementKey} ${elementType?elementType:''}on the ${tabPosition} tab with the "${textInput}" text`);
+
+        const elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
+        const pageIndex: number = Number(tabPosition.match(/\d/g)?.join('')) - 1;
+
+        await waitFor(async () => {
+            let pages = context.pages();
+            const result = await pages[pageIndex].waitForSelector(elementIdentifier, { state: 'visible' });
+            if (result) {
+                await fillTextOnPage(pages, pageIndex, elementIdentifier, textInput);
+            }
+            return result;
+        });
+    }
+)
