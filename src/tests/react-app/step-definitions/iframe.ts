261a36db3475c422f16c70cc84f39a21e50eaf75
Sergey Kolokolov <serg.a.bells@gmail.com>
2023-01-27 18:27:15 -0800

    adds `I fill the element in iframe with text` step

---
+++ src/tests/react-app/step-definitions/iframe.ts
@@ -0,0 +1,38 @@
+import { When } from '@cucumber/cucumber'
+import { ElementKey } from '../../../global-config'
+import { ScenarioWorld } from '../setup/world';
+import { waitFor } from '../../../resources/common/wait-for-behavior';
+import { getElementLocator } from '../../../resources/common/web-element-helper';
+import { getIframeElement, fillTextInIframe } from '../../../resources/common/html-behavior';
+
+When(
+    /^I fill the "([^"]*)" ([a-z]* )?in the "([^"]*)" iframe with the "(.*)" text$/,
+    async function(
+        this: ScenarioWorld,
+        elementKey: ElementKey, 
+        elementType: string,
+        iframeName: string,
+        textInput: string 
+    ) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+    
+        console.log(`I fill the ${elementKey} ${elementType?elementType:''}in the "${iframeName}" iframe with the "${textInput}" text`);
+
+        const elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
+        const iframeIdentifier: string = getElementLocator(page, iframeName, globalConfig);
+        const elementIframe = await getIframeElement(page, iframeIdentifier);
+
+        await waitFor( async () => {
+            const result = await page.waitForSelector(iframeIdentifier, { state: 'visible' });
+            if (result) {
+                if (elementIframe) {
+                    await fillTextInIframe(elementIframe, elementIdentifier, textInput);
+                }
+            }
+            return result;
+        });
+    }
+)
