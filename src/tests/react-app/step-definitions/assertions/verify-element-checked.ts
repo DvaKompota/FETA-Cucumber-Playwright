9fcbc4de5328655587c3501a54663190e4bb49be
Sergey Kolokolov <serg.a.bells@gmail.com>
2023-01-28 17:26:57 -0800

    adds element index support to element-checked step

--- src/tests/react-app/step-definitions/assertions/verify-element-checked.ts
+++ src/tests/react-app/step-definitions/assertions/verify-element-checked.ts
@@ -5,16 +5,27 @@ import { getElementLocator } from '../../../../resources/common/web-element-help
 import { waitFor } from '../../../../resources/common/wait-for-behavior';
 
 Then(
-    /^the "([^"]*)" should (not )?be checked$/,
-    async function( this: ScenarioWorld, elementKey: ElementKey, negate: boolean ) {
+    /^the( ([0-9]+st|[0-9]+nd|[0-9]+rd|[0-9]+th))? "([^"]*)"( [a-z]*)? should (not )?be checked$/,
+    async function(
+        this: ScenarioWorld,
+        elementPosition: string,
+        elementKey: ElementKey,
+        elementType: string,
+        negate: boolean,
+    ) {
         const {
             screen: { page },
             globalConfig,
         } = this;
 
-        console.log(`the ${elementKey} should ${negate?'not ':''}be checked`);
+        console.log(`The ${elementPosition?elementPosition+' ':''}${elementKey}${elementType?elementType:''} should ${negate?'not ':''}be checked`);
 
-        const elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
+        let elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
+
+        if (elementPosition) {
+            const elementIndex: number = Number(elementPosition.match(/\d+/)?.join('')) - 1;
+            elementIdentifier += ` >> nth=${elementIndex}`;
+        }
 
         await waitFor( async () => {
             const isElementChecked: boolean = await page.isChecked(elementIdentifier)
