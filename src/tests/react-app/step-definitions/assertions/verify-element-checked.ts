21c4c053da6f5988410bd5ce513e48401fc70c59
Sergey Kolokolov <serg.a.bells@gmail.com>
2023-02-18 16:45:03 -0800

    adds `element should be mui-checked` step

--- src/tests/react-app/step-definitions/assertions/verify-element-checked.ts
+++ src/tests/react-app/step-definitions/assertions/verify-element-checked.ts
@@ -3,6 +3,7 @@ import { ScenarioWorld } from '../../setup/world';
 import { ElementKey } from '../../../../global-config';
 import { getElementLocator } from '../../../../resources/common/web-element-helper';
 import { waitFor } from '../../../../resources/common/wait-for-behavior';
+import { getElementClass } from '../../../../resources/common/html-behavior';
 
 Then(
     /^the( ([0-9]+st|[0-9]+nd|[0-9]+rd|[0-9]+th))? "([^"]*)"( [a-z]*)? should (not )?be checked$/,
@@ -30,6 +31,38 @@ Then(
         await waitFor( async () => {
             const isElementChecked: boolean = await page.isChecked(elementIdentifier)
             return isElementChecked === !negate;
+        });
+
+    }
+)
+
+Then(
+    /^the( ([0-9]+st|[0-9]+nd|[0-9]+rd|[0-9]+th))? "([^"]*)"( [a-z]*)? should (not )?be mui-checked$/,
+    async function(
+        this: ScenarioWorld,
+        elementPosition: string,
+        elementKey: ElementKey,
+        elementType: string,
+        negate: boolean,
+    ) {
+        const {
+            screen: { page },
+            globalConfig,
+        } = this;
+
+        console.log(`The ${elementPosition?elementPosition+' ':''}${elementKey}${elementType?elementType:''} should ${negate?'not ':''}be mui-checked`);
+
+        let elementIdentifier: string = getElementLocator(page, elementKey, globalConfig);
+
+        if (elementPosition) {
+            const elementIndex: number = Number(elementPosition.match(/\d+/)?.join('')) - 1;
+            elementIdentifier += ` >> nth=${elementIndex}`;
+        }
+
+        await waitFor( async () => {
+            const elementClass: string | null = await getElementClass(page, elementIdentifier);
+            console.log(`elementClass 2 = ${elementClass}`);
+            return elementClass?.includes('Mui-checked') === !negate;
         });
 
     }
